74635f4b6551401c0a2dd08faa6e4d62f9689e94
Eddysasaki <eduardosasaki95@gmail.com>
2022-07-27 11:48:01 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import br.com.DIO.desafio.dominio.Curso;
-import br.com.DIO.desafio.dominio.Mentoria;
+import br.com.DIO.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso JAVA");
         curso1.setDescricao("Descrição curso JAVA");
@@ -15,12 +15,41 @@ public class Main {
         curso2.setDescricao("Descrição curso JAVASCRIPT");
         curso2.setCargaHoraria(4);
 
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria JAVA");
         mentoria.setDescricao("Descrição Mentoria JAVA");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição BootCamp JAVA developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devEduardo = new Dev();
+        devEduardo.setNome("Eduardo");
+        devEduardo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Eduardo" + devEduardo.getConteudosInscritos());
+        devEduardo.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos Eduardo" + devEduardo.getConteudosInscritos());
+        System.out.println("Contéudos Concluidos Eduardo" + devEduardo.getConteudosConcluidos());
+        System.out.println("XP:" + devEduardo.calcularTotalXP());
+
+        System.out.println("-------------------------");
+
+        Dev devSasaki = new Dev();
+        devSasaki.setNome("Sasaki");
+        devSasaki.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Sasaki" + devSasaki.getConteudosInscritos());
+        devSasaki.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos Sasaki" + devSasaki.getConteudosInscritos());
+        System.out.println("Contéudos Concluidos Sasaki" + devSasaki.getConteudosConcluidos());
+        System.out.println("XP:" + devEduardo.calcularTotalXP());
     }
 }
